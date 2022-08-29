52fe9098889af421393abddacaa46a108203a151
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-08-29 14:47:56 +0530

    functions for each control section added to PerformActions object

--- main/PerformActions.kt
+++ main/PerformActions.kt
@@ -5,38 +5,71 @@ import java.awt.Robot
 import java.awt.event.KeyEvent
 
 object PerformActions {
-    private val r = Robot()
+    private val robotControl = Robot()
+    private var shiftPressed = false
+
     fun performAction(control: Control) {
-        // TODO: use the recieved data to perform actions on the device.
-        r.keyPress(KeyEvent.VK_WINDOWS)
-        r.keyRelease(KeyEvent.VK_WINDOWS);
-
-
+        // TODO: use the received data to perform actions on the device.
         handleLeftJoystick(control.playerMovement)
+        handleRightJotStick(control.mouseAngle, control.mouseStrength)
+        handleGamePad(control.gamePad)
+        handleShift(control.shift)
     }
 
     private var previousArrowKey: Int? = null
 
+    /**
+     * Takes the responsibility of handling the actions of the left joystick
+     */
     fun handleLeftJoystick(joyStickControls: JoyStickControls) {
         println("key press called ${joyStickControls.name}")
         if (previousArrowKey != null) {
             println("key release initiated")
-            r.keyRelease(previousArrowKey!!)
+            robotControl.keyRelease(previousArrowKey!!)
             previousArrowKey = null
         }
         val currentKey: Int? =
-                when (joyStickControls) {
-                    STICK_RIGHT -> KeyEvent.VK_RIGHT
-                    STICK_UP -> KeyEvent.VK_UP
-                    STICK_LEFT -> KeyEvent.VK_LEFT
-                    STICK_DOWN -> KeyEvent.VK_DOWN
-                    RELEASE -> null
-                }
+            when (joyStickControls) {
+                STICK_RIGHT -> KeyEvent.VK_RIGHT
+                STICK_UP -> KeyEvent.VK_UP
+                STICK_LEFT -> KeyEvent.VK_LEFT
+                STICK_DOWN -> KeyEvent.VK_DOWN
+                RELEASE -> null
+            }
         if (currentKey != null) {
             // TODO: keyPress isn't being held
             println("key pressed - $currentKey")
-            r.keyPress(currentKey)
+            robotControl.keyPress(currentKey)
             previousArrowKey = currentKey
+        } else {
+            // TODO: check if release works
         }
+    }
+
+    fun handleRightJotStick(mouseAngle: Int, mouseStrength: Int) {
+        // TODO: use angle and strength to move mouse
+        robotControl.mouseMove(2000, 2000)
+    }
+
+    fun handleGamePad(padControls: PadControls) {
+        when (padControls) {
+            // TODO: perform necessary key presses with releases
+            PadControls.TRIANGLE -> {}
+            PadControls.SQUARE -> {}
+            PadControls.CIRCLE -> {}
+            PadControls.CROSS -> {}
+            PadControls.RELEASE -> {}
+        }
+    }
+
+    fun handleShift(shift: Boolean) {
+        if (!shiftPressed && shift) {
+            // TODO: press shift
+            println("shift pressed")
+        } else if (shiftPressed && !shift) {
+            // TODO: release shift
+            println("shift released")
+        }
+        shiftPressed = shift
     }
 }
