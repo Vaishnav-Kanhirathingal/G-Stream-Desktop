ec4cb6130d21ffde71c11273b9bc165dbf7bcf10
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-09-12 19:49:50 +0530

    get image from url separated into a new function, port creation functions added

--- src/main/kotlin/UI/Main.kt
+++ src/main/kotlin/UI/Main.kt
@@ -1,5 +1,4 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,67 +8,88 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.toAwtImage
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.graphics.toPainter
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.google.gson.Gson
+import connection.ConnectionData
+import java.awt.Dimension
+import java.awt.Toolkit
 import java.io.ByteArrayOutputStream
 import java.net.HttpURLConnection
+import java.net.Inet4Address
 import java.net.URL
 import javax.imageio.ImageIO
 
 @Composable
-@Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(
-                onClick = {
-                    text = "Hello, Desktop!"
-                },
-                content = {
-                    Text(text)
-                }
-            )
-
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }, content = {
+                Text(text)
+            })
             Image(
-                painter = loadNetworkImage().toAwtImage().toPainter(),
+                painter = getConnectionImageBitmap(),
                 contentDescription = "Scan this QRCode to connect to this game server to play games available on this device"
             )
         }
     }
 }
 
-
-fun loadNetworkImage(): ImageBitmap {
-    val size = 300
+fun getConnectionImageBitmap(): Painter {
     // TODO: data should be appropriate type
-    val data = "someData"
-    val url = URL("https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=$data")
-    val connection = url.openConnection() as HttpURLConnection
+    val screenSize: Dimension = Toolkit.getDefaultToolkit().screenSize
+    val data = Gson().toJson(
+        ConnectionData(
+            serverIpAddress = Inet4Address.getLocalHost().hostAddress,
+            controlPort = getControlPort(),
+            streamPort = getStreamingPort(),
+            horizontalResolution = screenSize.getWidth().toInt(),
+            verticalResolution = screenSize.getHeight().toInt(),
+            frameRateCap = 60,
+        )
+    )
+    val size = 400
+    val link = "https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=$data"
+    return getImageFromUrl(link = link)
+}
+
+fun getStreamingPort(): Int {
+    // TODO: open up a port and send back the details
+    return 0
+}
+
+fun getControlPort(): Int {
+    // TODO: open up a port and send back the details
+    return 0
+}
+
+fun getImageFromUrl(link: String): Painter {
+    val connection = URL(link).openConnection() as HttpURLConnection
     connection.connect()
-
-    val inputStream = connection.inputStream
-    val bufferedImage = ImageIO.read(inputStream)
-
     val stream = ByteArrayOutputStream()
-    ImageIO.write(bufferedImage, "png", stream)
-    val byteArray = stream.toByteArray()
-    return org.jetbrains.skia.Image.makeFromEncoded(byteArray).asImageBitmap()
+    ImageIO.write(ImageIO.read(connection.inputStream), "png", stream)
+    return org.jetbrains.skia.Image
+        .makeFromEncoded(stream.toByteArray())
+        .toComposeImageBitmap()
+        .toAwtImage()
+        .toPainter()
 }
 
 fun main() = application {
+    // TODO: get window icon url from mobile client
     Window(
+        title = "G-Stream",
         onCloseRequest = ::exitApplication,
-        resizable = false,
-    ) {
-        App()
-    }
+        resizable = false
+    ) { App() }
 }
