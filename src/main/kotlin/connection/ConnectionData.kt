bf1b22cf62e0514ebc6f2c3036db59122767a5a3
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-09-26 11:58:43 +0530

    connection data class modified

--- src/main/kotlin/connection/ConnectionData.kt
+++ src/main/kotlin/connection/ConnectionData.kt
@@ -3,8 +3,13 @@ package connection
 // TODO: check and optimize for necessary data
 class ConnectionData(
     val serverIpAddress: String,
-    val controlPort: Int,
+
     val streamPort: Int,
+
+    val movementPort: Int,
+    val gamePadPort: Int,
+    val mouseTrackPort: Int,
+    val shiftPort: Int,
 
     val horizontalResolution: Int,
     val verticalResolution: Int,
