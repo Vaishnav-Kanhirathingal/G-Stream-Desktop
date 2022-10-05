65f9daab1198863c06f5af858da3c57b3680061b
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-10-05 19:21:56 +0530

    test updated

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,41 +1,21 @@
-import java.awt.MouseInfo
-import java.awt.Robot
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.*
 
-var pastTime: Long? = null
-
-fun main() {
-    for (i in 0..10) {
-        val time = System.currentTimeMillis()
-        val gap = time - (pastTime ?: time)
-        pastTime = time
-        Thread.sleep(40)
-        val dx = (-400..400).random()
-        val dy = (-400..400).random()
-        animateToPosition(
-            dx = dx,
-            dy = dy,
-            timeGap = gap
-        )
-    }
+fun main(args: Array<String>) {
+    val list = NetworkInterface.networkInterfaces()
+    displayInterfaceInformation()
 }
 
-val robot = Robot()
+fun displayInterfaceInformation() {
+    for (netint in Collections.list(NetworkInterface.getNetworkInterfaces())) {
+        println("Display name:\t${netint.displayName}")
+        println("Name:\t${netint.name}")
 
-private fun animateToPosition(
-    dx: Int,
-    dy: Int,
-    frames: Int = 5,
-    timeGap: Long
-) {
-    val duration = if (timeGap > 50) 50 else timeGap
-    val perFrameTiming = duration / frames
-    println("perFrameTiming : $perFrameTiming, \t\ttimeGap : $timeGap, \t\tdx : $dx,\t\tdy : $dy")
-    val x = MouseInfo.getPointerInfo().location.x.toFloat()
-    val y = MouseInfo.getPointerInfo().location.y.toFloat()
-    for (i in 1..frames) {
-        Thread.sleep(perFrameTiming)
-        val fx = (x + (dx * i) / frames).toInt()
-        val fy = (y + (dy * i) / frames).toInt()
-        robot.mouseMove(fx, fy)
+        val inetAddresses: Enumeration<InetAddress> = netint.inetAddresses
+        for (inetAddress in Collections.list(inetAddresses)) {
+            println("InetAddress:\t${inetAddress.hostAddress}")
+        }
+        println("\n")
     }
 }
