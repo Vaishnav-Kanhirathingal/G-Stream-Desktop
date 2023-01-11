3fac8706b5bf67bd25ece7bdc499d0716fa261fc
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-01-11 12:03:29 +0530

    kotlin test file added

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,23 +1,53 @@
-import java.net.NetworkInterface
-import java.util.*
+import java.awt.MouseInfo
+import java.awt.Robot
+import java.time.LocalDateTime
+
+val robot = Robot()
 
 fun main(args: Array<String>) {
-    repeat(5) {
-        Thread.sleep(1000)
-        println("some")
+    Thread.sleep(10000)
+    for (i in 0..10000 step 50) {
+        Thread.sleep(20)
+        robot.mouseMove(
+            i,
+            MouseInfo.getPointerInfo().location.y
+        )//moves mouse to right by 100px
     }
 }
 
-fun displayInterfaceInformation(): String? {
-    for (netInterface in Collections.list(NetworkInterface.getNetworkInterfaces())) {
-        val listOfAddresses = Collections.list(netInterface.inetAddresses)
-        val isWlan = netInterface.name.contains("wlan")
-        if (listOfAddresses.isNotEmpty() && isWlan) {
-            for (inetAddress in listOfAddresses) {
-                if (inetAddress.hostAddress.startsWith("192.168"))
-                    return inetAddress.hostAddress
-            }
+fun testMouseDelta(dx: Int, dy: Int) {
+    robot.mouseMove(
+        MouseInfo.getPointerInfo().location.x + dx,
+        MouseInfo.getPointerInfo().location.y + dy
+    )
+}
+
+fun testInput() {
+    val currentTime = LocalDateTime.now().second
+    while (LocalDateTime.now().second < currentTime + 6) {
+        try {
+            robot.mouseMove(960 + 20, 540)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
-    return null
+}
+
+fun monitorMouseInput() {
+    repeat(1000) {
+        Thread.sleep(1000)
+        println("x - ${MouseInfo.getPointerInfo().location.x} \ty - ${MouseInfo.getPointerInfo().location.y}")
+    }
+}
+
+fun testSmall() {
+    Thread.sleep(3000)
+    val currentTime = LocalDateTime.now().second
+    while (LocalDateTime.now().second < currentTime + 6) {
+        try {
+            robot.mouseMove(960 + 1, 539)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
