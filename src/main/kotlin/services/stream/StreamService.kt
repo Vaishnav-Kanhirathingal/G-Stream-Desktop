f858580a2b4117d09e23d6005c3370ff0f8e9ae3
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-01-15 11:31:42 +0530

    basic outline for streaming services added

--- src/main/kotlin/services/stream/StreamService.kt
+++ src/main/kotlin/services/stream/StreamService.kt
@@ -1,4 +1,38 @@
 package services.stream
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.awt.Rectangle
+import java.awt.Robot
+import java.awt.image.BufferedImage
+import java.net.ServerSocket
+
+// TODO: try implementing video conferencing app
+
 class StreamService {
+    private val screenSocket = ServerSocket(0)
+    private val audioSocket = ServerSocket(0)
+
+    val screenPort get() = screenSocket.localPort
+    val audioPort get() = audioSocket.localPort
+
+    init {
+        val scope = CoroutineScope(Dispatchers.IO)
+
+        scope.launch { initiateVideoStreaming() }
+        scope.launch { initiateAudioStreaming() }
+    }
+
+    private suspend fun initiateVideoStreaming() {
+        val robot = Robot()
+        while (true) {
+            val bufferedImage: BufferedImage = robot.createScreenCapture(Rectangle())
+            // TODO: keep streaming video}
+        }
+    }
+
+    private suspend fun initiateAudioStreaming() {
+        // TODO: keep streaming audio
+    }
 }
