a5857e65ef1b4ebb310289b5dae94bf122240350
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-01-20 16:23:15 +0530

    testing screen capture

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,53 +1,65 @@
-import java.awt.MouseInfo
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import org.jetbrains.skia.EncodedImageFormat
+import org.jetbrains.skiko.toImage
+import java.awt.DisplayMode
+import java.awt.GraphicsEnvironment
+import java.awt.Rectangle
 import java.awt.Robot
-import java.time.LocalDateTime
+import java.io.File
+import javax.swing.JFrame
 
 val robot = Robot()
 
-fun main(args: Array<String>) {
-    Thread.sleep(10000)
-    for (i in 0..10000 step 50) {
-        Thread.sleep(20)
-        robot.mouseMove(
-            i,
-            MouseInfo.getPointerInfo().location.y
-        )//moves mouse to right by 100px
+val x = 800
+val y = 600
+
+fun setRes() {
+    val frame = JFrame()
+    frame.setSize(x, y)
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    val graphics = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        .defaultScreenDevice
+    graphics.fullScreenWindow = frame
+    graphics.displayMode = DisplayMode(x, y, 32, 60)
+    frame.isVisible = true
+}
+
+@Composable
+fun testImage() {
+    MaterialTheme {
+        Image(
+            bitmap = (Robot().createScreenCapture(Rectangle(800, 600))).toComposeImageBitmap(),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth().fillMaxHeight()
+        )
     }
 }
 
-fun testMouseDelta(dx: Int, dy: Int) {
-    robot.mouseMove(
-        MouseInfo.getPointerInfo().location.x + dx,
-        MouseInfo.getPointerInfo().location.y + dy
-    )
-}
+fun main() {
+    val folder = File("imageGallery/")
+    folder.mkdir()
+    println("${folder.exists()} name - ${folder.absolutePath}")
 
-fun testInput() {
-    val currentTime = LocalDateTime.now().second
-    while (LocalDateTime.now().second < currentTime + 6) {
-        try {
-            robot.mouseMove(960 + 20, 540)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-}
+    val time = System.currentTimeMillis()
 
-fun monitorMouseInput() {
-    repeat(1000) {
-        Thread.sleep(1000)
-        println("x - ${MouseInfo.getPointerInfo().location.x} \ty - ${MouseInfo.getPointerInfo().location.y}")
+    repeat(120) {
+        val data = robot
+            .createScreenCapture(Rectangle(1920, 1080))
+            .toImage()
+            .encodeToData(format = EncodedImageFormat.JPEG, quality = 10)
+            ?.bytes
+        val file = File(folder, "randomFile__$it.jpeg")
+        file.writeBytes(data!!)
+        println("${file.canWrite()} ${file.exists()} ${file.absolutePath}")
     }
-}
-
-fun testSmall() {
-    Thread.sleep(3000)
-    val currentTime = LocalDateTime.now().second
-    while (LocalDateTime.now().second < currentTime + 6) {
-        try {
-            robot.mouseMove(960 + 1, 539)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
+    val total = System.currentTimeMillis() - time
+    println("time taken = ${total}, mills per frame = ${total / 120}, frame rate = ${120000/total}")
 }
