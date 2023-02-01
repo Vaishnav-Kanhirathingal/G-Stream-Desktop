36ceaee8aa40e17543dabf48e585a0b135646b95
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-02-01 11:36:26 +0530

    changes to video streaming process and added audio server

--- src/main/kotlin/services/stream/StreamService.kt
+++ src/main/kotlin/services/stream/StreamService.kt
@@ -7,11 +7,10 @@ import org.jetbrains.skia.EncodedImageFormat
 import org.jetbrains.skiko.toImage
 import java.awt.Rectangle
 import java.awt.Robot
-import java.awt.geom.AffineTransform
-import java.awt.image.AffineTransformOp
 import java.awt.image.BufferedImage
 import java.io.DataOutputStream
 import java.net.ServerSocket
+import kotlin.random.Random
 
 class StreamService(
     val audioServerError: () -> Unit,
@@ -42,46 +41,54 @@ class StreamService(
         }
     }
 
-    private val outPutWidth = 1920 / 2
-    private val outPutHeight = 1080 / 2
+    private val outPutWidth = 1920 / 3
+    private val outPutHeight = 1080 / 3
 
     private suspend fun initiateVideoStreaming() {
         val outputStream = DataOutputStream(screenSocket.accept().getOutputStream())
         val robot = Robot()
-        while (true) {
+        try {
+            while (true) {
 
-            //---------------------------------------------------------------------------------------------------------||
-            val fullScaleImage = robot.createScreenCapture(Rectangle(1920, 1080))
+                //---------------------------------------------------------------------------------------------------------||
+                val fullScaleImage = robot.createScreenCapture(Rectangle(1920, 1080))
 
-            //---------------------------------------------------------------------------------------------------------24Max
-            val bufferedImage1 =
-                AffineTransformOp(AffineTransform.getScaleInstance(0.5, 0.5), AffineTransformOp.TYPE_BILINEAR).filter(
-                    fullScaleImage, BufferedImage(outPutWidth, outPutHeight, fullScaleImage.type)
-                )
+                //---------------------------------------------------------------------------------------------------------25Max
+                val bufferedImage = BufferedImage(outPutWidth, outPutHeight, BufferedImage.TYPE_INT_RGB)
+                val graphics = bufferedImage.createGraphics()
+                graphics.drawImage(fullScaleImage, 0, 0, outPutWidth, outPutHeight, null)
+                graphics.dispose()
 
-            //---------------------------------------------------------------------------------------------------------25Max
-            val bufferedImage2 = BufferedImage(outPutWidth, outPutHeight, BufferedImage.TYPE_INT_ARGB)
-            val graphics = bufferedImage2.createGraphics()
-            graphics.drawImage(fullScaleImage, 0, 0, outPutWidth, outPutHeight, null)
-            graphics.dispose()
+                val data = bufferedImage.toImage().encodeToData(EncodedImageFormat.JPEG, 60)
+                val jpegByteArray = data?.bytes!!
 
-            val data = bufferedImage2.toImage().encodeToData(EncodedImageFormat.JPEG, 60)
-            val jpegByteArray = data?.bytes!!
-
-            //---------------------------------------------------------------------------------------------------------||
-            outputStream.apply {
-                frames++
-                writeInt(jpegByteArray.size)
-                write(jpegByteArray)
-                flush()
+                //---------------------------------------------------------------------------------------------------------||
+                outputStream.apply {
+                    frames++
+                    writeInt(jpegByteArray.size)
+                    write(jpegByteArray)
+                    flush()
+                }
+                // TODO: capture a few consecutive frames with a specified interval combine the captured frames into a
+                //  packet using lossy compression send those frames to the mobile device
             }
-            // TODO: capture a few consecutive frames with a specified interval combine the captured frames into a
-            //  packet using lossy compression send those frames to the mobile device
+        } catch (e: Exception) {
+            videoServerError()
+            e.printStackTrace()
         }
     }
 
     private suspend fun initiateAudioStreaming() {
-        // TODO: keep streaming audio
+        val outputStream = DataOutputStream(audioSocket.accept().getOutputStream())
+        while (true) {
+            Thread.sleep(1000)
+            val str = "random str ${Random(100).nextInt()}"
+            println(str)
+            outputStream.apply {
+                writeUTF(str)
+                flush()
+            }
+        }
     }
 }
 
